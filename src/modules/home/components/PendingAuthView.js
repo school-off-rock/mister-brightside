324ad180c8b286d8305aca81f3b9c9867aa20c40
Bruno Talhate <btalhate@gmail.com>
2018-10-15 15:13:49 -0300

    [feature] added camera icon to camera authorization screen

--- src/modules/home/components/PendingAuthView.js
+++ src/modules/home/components/PendingAuthView.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { View } from 'react-native'
 
-import { styles } from '../styles/styles.home'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+import { COLORS, METRICS } from '../../../constants/theme'
 import { H6, Paragraph } from '../../shared/components/text'
 import { CAMERA_PERMISSION_TITLE, CAMERA_PERMISSION_MESSAGE } from '../../../constants/strings'
+
+import { styles } from '../styles/styles.home'
 
 export const PendingAuthView = () => {
   return (
     <View style={styles.pendingWrap}>
-      <H6>{CAMERA_PERMISSION_TITLE}</H6>
+      <Icon name="camera" color={COLORS.BLACK_SECONDARY_ALT} size={METRICS.ICONS.xxl} />
+      <H6 style={styles.pendingText}>{CAMERA_PERMISSION_TITLE}</H6>
       <Paragraph style={styles.pendingText}>{CAMERA_PERMISSION_MESSAGE}</Paragraph>
     </View>
   )

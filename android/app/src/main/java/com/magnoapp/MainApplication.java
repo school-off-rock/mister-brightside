9f91f0a06cdf7374f1312165c1b53413112b33ef
Bruno Talhate <btalhate@gmail.com>
2018-10-17 10:54:03 -0300

    [YARN REQUIRED] added RNblur package

--- android/app/src/main/java/com/magnoapp/MainApplication.java
+++ android/app/src/main/java/com/magnoapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.magnoapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.cmcewen.blurview.BlurViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlurViewPackage(),
             new VectorIconsPackage(),
             new BlurViewPackage(),
             new ReactNativeConfigPackage(),

c4778d26ada0fab7aadf7638734d1e20762b8e94
Bruno Talhate <btalhate@gmail.com>
2018-10-17 14:39:19 -0300

    [dev] added screen tracker for updating statusBar

--- src/config/redux.js
+++ src/config/redux.js
@@ -3,8 +3,9 @@ import { applyMiddleware, createStore as create } from 'redux'
 
 import { rootReducer } from '../redux/reducers'
 import { navMiddleware } from '../navigation/navigator'
+import screenTracking from './screenTracking'
 
-const middleware = [thunk, navMiddleware]
+const middleware = [thunk, navMiddleware, screenTracking]
 
 export const createStore = () => {
   return create(rootReducer, applyMiddleware(...middleware))

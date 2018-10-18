0705e1284a64bbfee3515437fce35b4cd28d1ded
Bruno Talhate <btalhate@gmail.com>
2018-10-18 15:23:49 -0300

    [feature] statusBar and splashScreen android

--- src/modules/shared/components/StatusBarLight.js
+++ src/modules/shared/components/StatusBarLight.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { StatusBar, Platform } from 'react-native'
 
 import { COLORS } from '../../../constants/theme'
 
 export const StatusBarLight = () => (
   <StatusBar
     backgroundColor={COLORS.ANDROID_STATUS_BAR}
-    barStyle="light-content"
+    barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
     translucent
   />
 )
--- src/modules/shared/components/hoc/withMainModal.js
+++ src/modules/shared/components/hoc/withMainModal.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StatusBar } from 'react-native'
 
 import { COLORS } from '../../../../constants/theme'
 import { ModalMain } from '../../containers/ModalMain'
 
 export const withModal = Component => ({ ...props }) => (
   <View style={{ flex: 1, backgroundColor: COLORS.SCREEN }}>
+    <StatusBar translucent backgroundColor={COLORS.ANDROID_STATUS_BAR} barStyle="dark-content" />
     <Component {...props} />
     <ModalMain />
   </View>

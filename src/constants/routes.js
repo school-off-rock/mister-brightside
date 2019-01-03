67ef8ae293da6716ac8eb73ec74734652ad7df36
Bruno Talhate <btalhate@gmail.com>
2019-01-03 14:11:58 -0200

    [dev] added clock in time check endpoint route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,4 @@
-import { Values } from './values'
+import { Values } from "./values"
 
 const { API_URL, FRAPI_API_URL, IP_ADDRESS_API } = Values
 
@@ -7,9 +7,13 @@ export const GET_IP_ADDRESS = `${IP_ADDRESS_API}?format=json`
 export const VERIFY_IP_ADDRESS = ipAddress => `${API_URL}ponto/ip/${ipAddress}`
 
 // MAGNO ID
-export const VERIFY_EMPLOYEE_REGISTRATION = reg => `${API_URL}funcionario?matricula=${reg}`
+export const VERIFY_EMPLOYEE_REGISTRATION = reg =>
+  `${API_URL}funcionario?matricula=${reg}`
 export const REGISTER_EMPLOYEE_ENTRY = `${API_URL}ponto/registrar`
-export const GET_EMPLOYEE_CLOCK_ENTRIES = (reg, initDate, endDate) => `${API_URL}ponto/registros/${reg}?dataInicio=${initDate}&dataFim=${endDate}`
+export const CHECK_EMPLOYEE_ENTRY_TIME = registration =>
+  `${API_URL}ponto/verificaescala?matricula=${registration}`
+export const GET_EMPLOYEE_CLOCK_ENTRIES = (reg, initDate, endDate) =>
+  `${API_URL}ponto/registros/${reg}?dataInicio=${initDate}&dataFim=${endDate}`
 
 // IMAGE CHECK
 export const TRAIN_EMPLOYEE_PHOTO = `${FRAPI_API_URL}train/person`

4cd719342150c133f52516a41f3ae9cf2dc2d911
Bruno Talhate <btalhate@gmail.com>
2019-04-03 16:13:37 -0300

    button contained component created

---
+++ src/modules/shared/components/buttons/ButtonContained.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Touchable from 'react-native-platform-touchable'
+import glamorous from 'glamorous-native'
+import { FONTS } from '../../../../constants/theme'
+import { hasText } from '../../../../config/functions'
+
+const ButtonContainer = glamorous.view(({ containerColor }, theme) => ({
+  borderRadius: theme.borderRadius,
+  overflow: 'hidden',
+  elevation: containerColor === 'none' ? 0 : 4,
+}))
+
+const ButtonWrap = glamorous.view(({ containerColor = 'primary' }, theme) => ({
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: theme.colors[containerColor],
+  paddingVertical: theme.button.verticalSpacing,
+  paddingHorizontal: theme.button.horizontalSpacing,
+}))
+
+const ButtonText = glamorous.text((props, theme) => ({
+  color: theme.colors.background.onPrimary,
+  ...FONTS.style.primaryButton,
+}))
+
+export function ButtonContained({ children, onPress, style, color }) {
+  const upperCaseText = hasText(children) ? children.toUpperCase() : children
+  return (
+    <ButtonContainer containerColor={color} style={style}>
+      <Touchable
+        onPress={onPress}
+        foreground={Touchable.SelectableBackground()}
+      >
+        <ButtonWrap containerColor={color}>
+          <ButtonText>{upperCaseText}</ButtonText>
+        </ButtonWrap>
+      </Touchable>
+    </ButtonContainer>
+  )
+}

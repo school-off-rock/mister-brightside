c3527f7a9b0019bd3ad9b8308541565e83c9fcf0
Bruno Talhate <btalhate@gmail.com>
2019-04-05 13:35:51 -0300

    fab with correct ripple

--- src/modules/shared/components/FAB.js
+++ src/modules/shared/components/FAB.js
@@ -1,26 +1,45 @@
 import React from 'react'
+import { Platform } from 'react-native'
 import glamorous from 'glamorous-native'
 import Touchable from 'react-native-platform-touchable'
 
-export const FabWrap = glamorous.view((props, theme) => ({
-  borderRadius: theme.fabRadius,
-  overflow: 'hidden',
-  elevation: 6,
-}))
+const hasRipple = Platform.OS === 'android' && Platform.Version > 19
 
-export const FabBackground = glamorous.view((props, theme) => ({
-  backgroundColor: theme.colors.primary,
-  width: theme.fabSize,
-  aspectRatio: theme.ratio.square,
-  // borderRadius: theme.fabRadius,
-  alignItems: 'center',
-  justifyContent: 'center',
-}))
+export const FabWrap = glamorous.view(
+  (props, theme) =>
+    hasRipple && {
+      borderRadius: theme.fabRadius,
+      overflow: 'hidden',
+      elevation: 6,
+      backgroundColor: theme.colors.primary,
+    }
+)
+
+export const FabBackground = glamorous.view((props, theme) =>
+  hasRipple
+    ? {
+        width: theme.fabSize,
+        aspectRatio: theme.ratio.square,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }
+    : {
+        backgroundColor: theme.colors.primary,
+        width: theme.fabSize,
+        borderRadius: theme.fabRadius,
+        aspectRatio: theme.ratio.square,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }
+)
 
 export function FAB({ onPress, children }) {
   return (
     <FabWrap>
-      <Touchable onPress={onPress}>
+      <Touchable
+        foreground={Touchable.SelectableBackground()}
+        onPress={onPress}
+      >
         <FabBackground>{children}</FabBackground>
       </Touchable>
     </FabWrap>
